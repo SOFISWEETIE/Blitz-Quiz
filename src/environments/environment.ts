233489068b63f7aae98ff63a772edc812623d4fe
SOFISWEETIE <strator03@gmail.com>
2025-11-09 20:22:32 +0100

    Cree la carpeta environment con la conexion a firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// para conectar con firebase
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCisEzjaNmpuOxdHna0qxJZSrdxI8jmCX8",
+    authDomain: "sas-tfg.firebaseapp.com",
+    projectId: "sas-tfg",
+    storageBucket: "sas-tfg.firebasestorage.app",
+    messagingSenderId: "737848605061",
+    appId: "1:737848605061:web:fd22d3ba550dadbc772cec",
+    measurementId: "G-63C7RV1D8G"
+  }
+};

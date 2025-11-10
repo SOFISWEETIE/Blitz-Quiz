5d992ba5a8a4b1a756202a66cfed35cd91293397
Alejandro Pimentel Pacheco <alexpimentelpacheco@gmail.com>
2025-11-10 15:42:59 +0100

    Agrega script env-to-ts, environment.ts de ejemplo y actualiza .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+production: false,
+apiUrl: 'http://localhost:3000/api',
+authToken: '',
+firebaseApiKey: 'AIzaSyCisEzjaNmpuOxdHna0qxJZSrdxI8jmCX8',
+firebaseAuthDomain: 'sas-tfg.firebaseapp.com'
+};

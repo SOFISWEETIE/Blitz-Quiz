1348ffed9f698de0d11ba7150b327a435a297935
SOFISWEETIE <strator03@gmail.com>
2025-11-14 15:31:11 +0100

    comentarios añadidos

--- src/app/servicios/pregunta.servicio.ts
+++ src/app/servicios/pregunta.servicio.ts
@@ -3,15 +3,21 @@ import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Pregunta } from '../modelos/pregunta.modelo';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class PreguntaServicio {
 
+// insertamos firestore
   constructor(private firestore: Firestore) { }
 
+  
+  
+// rescatamos las preguntas desde las preguntas
   obtenerPreguntas(): Observable<Pregunta[]> {
     const ref = collection(this.firestore, 'preguntas');
+  // devuelve el objeto preguntas
     return collectionData(ref, { idField: 'id' }) as Observable<Pregunta[]>;
   }
 }

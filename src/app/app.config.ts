c6c166e87b154a0582005df718ad7b29db0c03c4
SOFISWEETIE <strator03@gmail.com>
2025-11-26 22:37:55 +0100

    juego y login funcionandoooo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,21 +2,27 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp({
-      apiKey: "A............8",
-      authDomain: "s.......com",
-      projectId: "s.....g",
-      storageBucket: "s......p",
-      messagingSenderId: "7.....1",
-      appId: "1....cec"
-    })),
+      
+  apiKey: "AIzaSyCisEzjaNmpuOxdHna0qxJZSrdxI8jmCX8",
+  authDomain: "sas-tfg.firebaseapp.com",
+  projectId: "sas-tfg",
+  storageBucket: "sas-tfg.firebasestorage.app",
+  messagingSenderId: "737848605061",
+  appId: "1:737848605061:web:fd22d3ba550dadbc772cec",
+  measurementId: "G-63C7RV1D8G"
+}))
+
+    ,  // ← lee del env
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
   ]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyCisEzjaNmpuOxdHna0qxJZSrdxI8jmCX8",
-    authDomain: "sas-tfg.firebaseapp.com",
-    projectId: "sas-tfg",
-    storageBucket: "sas-tfg.firebasestorage.app",
-    messagingSenderId: "737848605061",
-    appId: "1:737848605061:web:fd22d3ba550dadbc772cec"
-  }
+export const firebaseConfig = {
+  apiKey: "AIzaSyCisEzjaNmpuOxdHna0qxJZSrdxI8jmCX8",
+  authDomain: "sas-tfg.firebaseapp.com",
+  projectId: "sas-tfg",
+  storageBucket: "sas-tfg.firebasestorage.app",
+  messagingSenderId: "737848605061",
+  appId: "1:737848605061:web:fd22d3ba550dadbc772cec",
+  measurementId: "G-63C7RV1D8G"
 };

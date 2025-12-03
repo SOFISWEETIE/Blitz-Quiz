a3a042a3125b256d39426553b8e8241020c7b325
Al3jandroDev <157000579+Al3jandroDev@users.noreply.github.com>
2025-12-03 14:37:30 +0100

    Refactor Firebase initialization with environment config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,16 @@
-// src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-
-
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideFirebaseApp(() => initializeApp({
-      
-  apiKey: "AIzaSyCisEzjaNmpuOxdHna0qxJZSrdxI8jmCX8",
-  authDomain: "sas-tfg.firebaseapp.com",
-  projectId: "sas-tfg",
-  storageBucket: "sas-tfg.firebasestorage.app",
-  messagingSenderId: "737848605061",
-  appId: "1:737848605061:web:fd22d3ba550dadbc772cec",
-  measurementId: "G-63C7RV1D8G"
-}))
-
-    ,  // ← lee del env
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideRouter(routes)
   ]
 };

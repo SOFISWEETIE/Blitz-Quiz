ea34a4bd1708a79cf379f7bfc79edab81718a475
Al3jandroDev <157000579+Al3jandroDev@users.noreply.github.com>
2025-12-09 15:24:10 +0100

    Implementar AliasGuard

---
+++ src/app/servicios/alias.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../servicios/auth.service';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AliasGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate() {
+    return this.auth.verificarAlias().pipe(
+      map(alias => {
+        if (alias) return true;
+
+        this.router.navigate(['/crear-alias']);
+        return false;
+      })
+    );
+  }
+}

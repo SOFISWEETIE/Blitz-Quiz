e4713ec3c532fbeeae04c1936260aead8d8eb647
Al3jandroDev <157000579+Al3jandroDev@users.noreply.github.com>
2025-12-09 15:23:03 +0100

    Implementar alias
    Add alias management to LayoutComponent

--- src/app/componentes/layout.component/layout.component.ts
+++ src/app/componentes/layout.component/layout.component.ts
@@ -4,6 +4,8 @@ import { RouterOutlet } from '@angular/router';
 import { AuthService } from '../../servicios/auth.service';
 import { Router } from '@angular/router';
 import { RankingComponent } from '../ranking.component/ranking.component';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout',
@@ -13,12 +15,28 @@ import { RankingComponent } from '../ranking.component/ranking.component';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
-  constructor(public auth: AuthService, private router: Router) {}
+
+   alias$: Observable<string | null> = of(null);
+  
+  constructor(public auth: AuthService, private router: Router) {
+    
+    // obtener el alias al iniciar la aplicación
+    this.alias$ = this.auth.user$.pipe(
+      switchMap(user => {
+        if (!user) return of(null);
+        return this.auth.verificarAlias();
+      })
+    );
+  }
 
   //metodo para cerrar sesion
   async salir() {
     await this.auth.logout();               // cierra sesion en Firebase
     await this.router.navigate(['/login']);  // redirige al login
+  }
+
+  cambiarAlias() {
+    this.router.navigate(['/app/crear-alias']);
   }
 
 }

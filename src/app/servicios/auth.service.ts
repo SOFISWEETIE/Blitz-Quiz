283f7135958c668b176a600855a4ca151e8a6639
Al3jandroDev <157000579+Al3jandroDev@users.noreply.github.com>
2025-12-09 15:27:12 +0100

    Implementar verificación del alias
    Added methods to verify and save user alias in Firestore.

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,14 +1,16 @@
 // src/app/servicios/auth.service.ts
 import { Injectable } from '@angular/core';
 import { Auth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, User } from '@angular/fire/auth';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, switchMap, of } from 'rxjs';
 import { inject } from '@angular/core';
+import { Firestore, doc, getDoc, setDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private auth: Auth = inject(Auth);  // ← INYECTAMOS CON inject() EN VEZ DE CONSTRUCTOR
+  private firestore: Firestore = inject(Firestore); 
   user$ = new BehaviorSubject<User | null>(null);
 
   constructor() {
@@ -26,4 +28,26 @@ export class AuthService {
   async logout() {
     return signOut(this.auth);
   }
+
+   verificarAlias() {
+    return this.user$.pipe(
+      switchMap(user => {
+        if (!user) return of(null);
+
+        const ref = doc(this.firestore, 'usuarios', user.uid);
+        return getDoc(ref).then(d => d.exists() ? d.data()['alias'] : null);
+      })
+    );
+  }
+
+
+   guardarAlias(uid: string, alias: string) {
+    const ref = doc(this.firestore, 'usuarios', uid);
+    return setDoc(ref, { alias }, { merge: true }).then(() => {
+      const user = this.user$.value;
+      if (user) this.user$.next(user); // forzar la actualización
+  });
+}
+
+
 }
